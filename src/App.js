a4d5ebf7d8a6676933380f493df050e6e9864c5a
Sofia Lynch <113479969+SofiaBLynch@users.noreply.github.com>
2024-02-10 12:55:57 -0500

    Basic Background

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-
+    <div class="App">
+      <div class='App-header'>
+        <h1>Grocery Guru</h1>
+        <p>Learn how to financially plan for groceries. Input the ingredients you need for your reciepe and find out the cost to make the meal</p>
+      </div>
+      <div class='userInput'>
+        <p> adklfjadsl</p>
+      </div>
     </div>
   );
 }

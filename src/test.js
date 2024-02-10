3617c071298575481ae594fe123ead2d747a12bd
J <88823380+jijirepo@users.noreply.github.com>
2024-02-10 12:29:14 -0500

    test website (not seperated)

---
+++ src/test.js
@@ -0,0 +1,28 @@
+function Test() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input type="text" value="this is a test" />
+      <input type="text" value="this is a test" />
+      
+      {/* half size */}
+      <img 
+        src="https://rotatingsandwiches.com/wp-content/uploads/2023/03/half-smoke.gif?w=480b" 
+        alt="Half-Smoke GIF" 
+        style={{ width: '240px' }} // Half of 480px
+      />
+
+      {/* Second half size */}
+      <img 
+        src="https://rotatingsandwiches.com/wp-content/uploads/2023/02/pork-gyro.gif?w=480" 
+        alt="GYRO GIF" 
+        style={{ width: '240px' }} // Half of 480px
+      />
+
+      {/* Buttons */}
+      <button>Click Me!!</button>
+    </div>
+  );
+}
+
+export default Test;
